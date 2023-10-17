0d17ea8e06811571d77a9a08ead265ee94157b7a
millamzy <ndefoago@gmail.com>
2023-10-17 14:12:17 +0100

    added my certificate

--- index.js
+++ index.js
@@ -1,72 +1,3 @@
-
-// toggle icon navbar
-let menuIcon=document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
-
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-
-window.onscroll = () => {
-     sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset =sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >=offset && top < offset + height) {
-            navLinks.forEach(links=>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
-            });
-        };
-        
-     });
-     // =========sticky navbar============
-     let header = document.querySelector('header');
-     header.classList.toggle('sticky', window.scrollY > 100);
-        //  remove toggleicon and navbar
-
-        menuIcon.classList.remove('bx-x');
-        navbar.classList.remove('active');
-
-};
-// ==================scroll reveal==============
-   ScrollReveal({ 
-    // reset: true,
-    distance:'80px',
-    duration:2000,
-    delay:200
- });
-
- ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
- ScrollReveal().reveal('.home-img, .services-container,.portfolio-box, .contact form',  { origin: 'botton' });
-
- ScrollReveal().reveal('.home-content h1, .about-img',  { origin: 'left' });
- ScrollReveal().reveal('.home-content p, .about-content',  { origin: 'right' });
- 
- 
-//  ==============typed js================
-
-const typed = new Typed('.multiple-text',{
-    strings:['Fullstack Developer','Graphic Designer','Product Designer'],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-});
-
-
-
-///=============EMAILJS=======
-
-
 const contactform = document.querySelector("#form");
 const submitBtn = document.querySelector(".btn");
 const nameinput = document.querySelector("#name");
@@ -75,7 +6,7 @@ const emailSubjectInput = document.querySelector("#emailsubject");
 const phoneNumberInput = document.querySelector("#number");
 const messageInput = document.querySelector("#message");
 
-const serviceID = "default_service";
+const serviceID = "service_mn6ymsb";
 const templateID = "template_4se3hrt"
 
 emailSubjectInput.init(publickey)
